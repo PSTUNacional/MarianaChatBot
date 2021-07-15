8687e748e3b8d655b4be97a66fd0704ab71ab6ca
Marcel <marcelgiglio@users.noreply.github.com>
2021-07-15 02:37:21 -0300

    Update twitterBot.php
    Fiz algumas alterações, mas não consegui fazer testes. Então recomendo testar antes de rodar.
    - Arrumei a identação.
    - Alterei a linha que salva o last_id, porque estava pegando o primeiro, e não o último.
    - Alterei a função find_first_key_word para ela normalizar os vocábulos do texto para uma única palavra-chave.
    - Alterei a função key_word_phrase para ela ter várias opções de frase para cada palavra-chave e escolher uma delas aleatoriamente.

--- twitterBot.php
+++ twitterBot.php
@@ -1,8 +1,8 @@
 <?php
 	require_once( 'config.php' );
-	/*---------- MONTA A CONEXÃO COM A API ----------*/
 	require_once( 'TwitterAPIExchange.php' );
-
+	
+	/*---------- MONTA A CONEXÃO COM A API ----------*/
 	$settings = array(
 		'oauth_access_token' => TWITTER_ACCESS_TOKEN, 
 		'oauth_access_token_secret' => TWITTER_ACCESS_TOKEN_SECRET, 
@@ -10,77 +10,77 @@
 		'consumer_secret' => TWITTER_CONSUMER_SECRET,
 	);
 	
-	
-	
+	/*---------- Verifica o último id ----------*/
 	$file = fopen("last_id.txt", "r");
         while(! feof($file)) {
-        $last_id = fgets($file);
+        	$last_id = fgets($file);
     	}
 	fclose($file);
 	
-	/*---------- PUXA AS ÚLTIMAS MENÇÕE AO BOT ----------*/
+	/*---------- PUXA AS ÚLTIMAS MENÇÕEs AO BOT ----------*/
 	$url = 'https://api.twitter.com/1.1/statuses/mentions_timeline.json';
-    $requestMethod = 'GET';
-    $apiData = '?since_id='.$last_id;
-    $twitter = new TwitterAPIExchange( $settings );
-    $twitter->buildOauth($url, $requestMethod);
+	$requestMethod = 'GET';
+	$apiData = '?since_id='.$last_id;
+	$twitter = new TwitterAPIExchange( $settings );
+	$twitter->buildOauth($url, $requestMethod);
 	$response = $twitter->performRequest(true, array (CURLOPT_SSL_VERIFYHOST => 0, CURLOPT_SSL_VERIFYPEER => 0));
 	$lista = json_decode($response);
 	
 	/*---------- FUNÇÕES INTERPRETATIVAS - Busca no texto palavras-chave para condicionar a resposta  ----------*/
 	function find_first_key_word($tweet) {
-	$key_words = array("jornal", "filia", "filiação", "filiar");
-	foreach ($key_words as $word) {
-		if (strpos($tweet, $word) !== false) {
-			 return $word;
+		$key_words = array(
+			"jornal" => array("jornal"),
+			"filiar" => array("filiar", "filia", "filiação")
+		);
+		foreach ($key_words as $normalized_word => $possible_words) {
+			foreach ($possible_words as $word){
+				if (strpos($tweet, $word) !== false) {
+					return $normalized_word;
+				}
+			}
 		}
-	}
-	return NULL;
-    }
-
+		return NULL;
+    	}
+	
 	function key_word_phrase($keyword) {
-	
-	if(is_null($keyword))
-	{
-	    return "Oi! Me chamou? Não consigo responder agora. Eu e os programadores ainda estamos trabalhando nisso. Aproveite para dar uma olhada no nosso site https://pstu.org.br";
-	    
+		if(is_null($keyword)){
+			return "Oi! Me chamou? Não consigo responder agora. Eu e os programadores ainda estamos trabalhando nisso. Aproveite para dar uma olhada no nosso site https://pstu.org.br";
+		}
+		$phrases = array(
+			"jornal" => array(
+				"Oi, nesse link você acessa as últimas edições do jornal Opinião Socialista https://pstu.org.br/opiniaosocialista/",
+				"Esse último jornal está muito bom mesmo, recomendo! Dá uma olhada nas últimas edições nesse link https://pstu.org.br/opiniaosocialista/",
+				"Acredita que eu estava agora mesmo dando uma olhada no jornal? Aqui estão as últimas edições https://pstu.org.br/opiniaosocialista/"
+			),
+			"filiar" => array(
+				"Que demais! Fico muito feliz por você querer se filiar! Preenche seu cadastro nesse link, é rapidinho! Em breve alguém entrará em contato para terminar o processo. https://facaparte.pstu.org.br",
+				"AAAAaaaaahhh fico feliz que você está se filiando! Olha, só faz o cadastro nesse link e em breve alguém entrará em contato! https://facaparte.pstu.org.br",
+				"Meeee pareceee. Meeee pareceee. Meeee pareceee. Que o socialismo cresce. Faz o cadastro nesse link que eu to te mandando, e alguém entra em contato com você em breve! https://facaparte.pstu.org.br"
+			)
+		);
+		$statement = array_rand($phrases[$keyword]);
+		return $phrases[$keyword][$statement];
 	}
 	
-	$phrases = array(
-	    "jornal" =>     "Oi, nesse link você acessa as últimas edições do jornal Opinião Socialista https://pstu.org.br/opiniaosocialista/",
-	    "filia" =>      "Oi, nesse link você pode se cadastrar para filiação. Preenche lá, é rapidinho! Em breve alguém entrará em contato para terminar o processo. https://facaparte.pstu.org.br",
-    	"filiação" =>   "Oi, nesse link você pode se cadastrar para filiação. Preenche lá, é rapidinho! Em breve alguém entrará em contato para terminar o processo. https://facaparte.pstu.org.br",
-	    "filiar" =>     "Oi, nesse link você pode se cadastrar para filiação. Preenche lá, é rapidinho! Em breve alguém entrará em contato para terminar o processo. https://facaparte.pstu.org.br"
-	);
-	
-	return $phrases[$keyword];
-}
-	
-    /*---------- LOOPING - Responde os tweets ----------*/
+	/*---------- LOOPING - Responde os tweets ----------*/
 	foreach ($lista as $id_t){
-	    $id = $id_t->id;
-	    $tweet = $id_t->text;
-	    
-	    $urlAnswer = 'https://api.twitter.com/1.1/statuses/update.json';
-        $requestMethod = 'POST';
-    
-         $apiId = array(
-            'status' => key_word_phrase(find_first_key_word($tweet)),
-            'auto_populate_reply_metadata' => 'true',
-            'in_reply_to_status_id' => $id,
-            );
-        
-
-        $twitter = new TwitterAPIExchange( $settings );
-        $twitter->setPostfields($apiId);    
-        $twitter->buildOauth($urlAnswer, $requestMethod);
-    	$response = $twitter->performRequest(true, array (CURLOPT_SSL_VERIFYHOST => 0, CURLOPT_SSL_VERIFYPEER => 0));
+		$id = $id_t->id;
+		$tweet = $id_t->text;
+		$urlAnswer = 'https://api.twitter.com/1.1/statuses/update.json';
+		$requestMethod = 'POST';
+		$apiId = array(
+			'status' => key_word_phrase(find_first_key_word($tweet)),
+			'auto_populate_reply_metadata' => 'true',
+			'in_reply_to_status_id' => $id,
+		);
+		$twitter = new TwitterAPIExchange( $settings );
+		$twitter->setPostfields($apiId);
+		$twitter->buildOauth($urlAnswer, $requestMethod);
+		$response = $twitter->performRequest(true, array (CURLOPT_SSL_VERIFYHOST => 0, CURLOPT_SSL_VERIFYPEER => 0));
 	}
 	
-	
-    /*---------- REGISTRA LAST_ID - Salva o ID do último tweet respondido. Ele será o ponto de início do próximo ciclo ----------*/
-	
+	/*---------- REGISTRA LAST_ID - Salva o ID do último tweet respondido. Ele será o ponto de início do próximo ciclo ----------*/
 	$fp = fopen('last_id.txt', 'w');
-    fwrite($fp, $lista[0]->id);
-    fclose($fp);
-
+	//fwrite($fp, $lista[0]->id); //esse código pega o primeiro twitte respondido, não o último.
+	fwrite($fp, end($lista)->id);
+	fclose($fp);
